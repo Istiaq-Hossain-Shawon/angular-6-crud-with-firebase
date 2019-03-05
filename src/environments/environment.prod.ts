d233a6c4676883e856776ece67a33d9c9df3170e
Istiaq-hossain-shawon <hossain.ict@gmail.com>
2019-03-05 13:47:27 +0600

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAdodx--WUS6lDqVhHYG9BJbXRToLzzEiU",
+    authDomain: "angular-7-crud-with-firebase.firebaseapp.com",
+    databaseURL: "https://angular-7-crud-with-firebase.firebaseio.com",
+    projectId: "angular-7-crud-with-firebase",
+    storageBucket: "angular-7-crud-with-firebase.appspot.com",
+    messagingSenderId: "987878922791"
+  }
 };
